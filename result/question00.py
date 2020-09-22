c7068b55a21d316bbb5d2c33fca2eaf1634b5688
joq0033 <56523197+joq0033@users.noreply.github.com>
2020-09-22 14:37:39 +1200

    Add files via upload

--- result/question00.py
+++ result/question00.py
@@ -1,49 +1,1 @@
-import unittest
-from aquarium import Aquarium
-from owner import Owner
-
-
-class TestStaringAquariumCode(unittest.TestCase):
-
-    def test_a_aquarium_collection(self):
-        aquarium = Aquarium()
-        assert hasattr(aquarium, 'all_my_owners')
-        assert type(aquarium.all_my_owners) == list
-
-    def test_b_aquarium_add_owner(self):
-        aquarium = Aquarium()
-        assert hasattr(aquarium, 'add_owner')
-        assert callable(getattr(aquarium, 'add_owner', None))
-
-    def test_c_aquarium_find_owner(self):
-        aquarium = Aquarium()
-        assert hasattr(aquarium, 'find_owner')
-        assert callable(getattr(aquarium, 'find_owner', None))
-
-    def test_d_aquarium_sort_owners(self):
-        aquarium = Aquarium()
-        assert hasattr(aquarium, 'sort_owners')
-        assert callable(getattr(aquarium, 'sort_owners', None))
-
-
-class TestStaringOwnerCode(unittest.TestCase):
-    def test_a_owner_properties(self):
-        aquarium = Aquarium()
-        owner = Owner('id', 'first_name', 'last_name', 'birth_date', aquarium)
-        assert hasattr(owner, 'id')
-        assert hasattr(owner, 'first_name')
-        assert hasattr(owner, 'last_name')
-        assert hasattr(owner, 'birth_date')
-        assert hasattr(owner, 'my_aquarium')
-        assert hasattr(owner, 'all_my_fish')
-        assert type(owner.all_my_fish) == list
-
-    def test_b_owner_methods(self):
-        aquarium = Aquarium()
-        owner = Owner('id', 'first_name', 'last_name', 'birth_date', aquarium)
-        assert hasattr(owner, 'sort_fish')
-        assert callable(getattr(owner, 'sort_fish', None))
-
-
-if __name__ == '__main__':
-    unittest.main(verbosity=3)
+import pytestfrom aquarium import Aquariumfrom owner import Ownerclass TestStaringAquariumCode:    def test_a_aquarium_collection(self):        aquarium = Aquarium()        assert hasattr(aquarium, 'all_my_owners')        assert type(aquarium.all_my_owners) == list    def test_b_aquarium_add_owner(self):        aquarium = Aquarium()        assert hasattr(aquarium, 'add_owner')        assert callable(getattr(aquarium, 'add_owner', None))    def test_c_aquarium_find_owner(self):        aquarium = Aquarium()        assert hasattr(aquarium, 'find_owner')        assert callable(getattr(aquarium, 'find_owner', None))    def test_d_aquarium_sort_owners(self):        aquarium = Aquarium()        assert hasattr(aquarium, 'sort_owners')        assert callable(getattr(aquarium, 'sort_owners', None))class TestStaringOwnerCode:    def test_a_owner_properties(self):        aquarium = Aquarium()        owner = Owner('id', 'first_name', 'last_name', 'birth_date', aquarium)        assert hasattr(owner, 'id')        assert hasattr(owner, 'first_name')        assert hasattr(owner, 'last_name')        assert hasattr(owner, 'birth_date')        assert hasattr(owner, 'my_aquarium')        assert hasattr(owner, 'all_my_fish')        assert type(owner.all_my_fish) == list    def test_b_owner_methods(self):        aquarium = Aquarium()        owner = Owner('id', 'first_name', 'last_name', 'birth_date', aquarium)        assert hasattr(owner, 'sort_fish')        assert callable(getattr(owner, 'sort_fish', None))if __name__ == '__main__':    pytest.main(['-v', '-s', 'question00.py'])
--- result/question06.py
+++ result/question06.py
@@ -1,44 +1,1 @@
-import unittest
-from aquarium import Aquarium
-from owner import Owner
-
-
-class TestOwnerHasOneFish(unittest.TestCase):
-    def test_a_owner_has_two_fish_exists(self):
-        aquarium = Aquarium()
-        owner = Owner('id', 'first_name', 'last_name', 'birth_date', aquarium)
-        assert hasattr(owner, 'has_two_fish')
-        assert callable(getattr(owner, 'has_two_fish', None))
-
-    def test_b_has_two_fish_returns_false_with_0_fish(self):
-        aquarium = Aquarium()
-        owner = Owner('id', 'first_name', 'last_name', 'birth_date', aquarium)
-        returned = owner.has_two_fish()
-        assert returned == False
-
-    def test_c_has_two_fish_returns_false_with_1_fish(self):
-        aquarium = Aquarium()
-        owner = Owner('id', 'first_name', 'last_name', 'birth_date', aquarium)
-        owner.add_fish('height', 'color', 'species', 'gender')
-        returned = owner.has_two_fish()
-        assert returned == False
-
-    def test_d_has_two_fish_returns_true_with_2_fish(self):
-        aquarium = Aquarium()
-        owner = Owner('id', 'first_name', 'last_name', 'birth_date', aquarium)
-        owner.add_fish('height', 'color', 'species', 'gender')
-        owner.add_fish('height', 'color', 'species', 'gender')
-        returned = owner.has_two_fish()
-        assert returned == True
-
-    def test_e_has_two_fish_returns_false_with_3_fish(self):
-        aquarium = Aquarium()
-        owner = Owner('id', 'first_name', 'last_name', 'birth_date', aquarium)
-        owner.add_fish('height', 'color', 'species', 'gender')
-        owner.add_fish('height', 'color', 'species', 'gender')
-        owner.add_fish('height', 'color', 'species', 'gender')
-        returned = owner.has_two_fish()
-        assert returned == False
-
-if __name__ == '__main__':
-    unittest.main(verbosity=3)
+import pytestfrom aquarium import Aquariumfrom owner import Ownerclass TestOwnerHasOneFish:    def test_a_owner_has_two_fish_exists(self):        aquarium = Aquarium()        owner = Owner('id', 'first_name', 'last_name', 'birth_date', aquarium)        assert hasattr(owner, 'has_two_fish')        assert callable(getattr(owner, 'has_two_fish', None))    def test_b_has_two_fish_returns_false_with_0_fish(self):        aquarium = Aquarium()        owner = Owner('id', 'first_name', 'last_name', 'birth_date', aquarium)        returned = owner.has_two_fish()        assert returned == False    def test_c_has_two_fish_returns_false_with_1_fish(self):        aquarium = Aquarium()        owner = Owner('id', 'first_name', 'last_name', 'birth_date', aquarium)        owner.add_fish('height', 'color', 'species', 'gender')        returned = owner.has_two_fish()        assert returned == False    def test_d_has_two_fish_returns_true_with_2_fish(self):        aquarium = Aquarium()        owner = Owner('id', 'first_name', 'last_name', 'birth_date', aquarium)        owner.add_fish('height', 'color', 'species', 'gender')        owner.add_fish('height', 'color', 'species', 'gender')        returned = owner.has_two_fish()        assert returned == True    def test_e_has_two_fish_returns_false_with_3_fish(self):        aquarium = Aquarium()        owner = Owner('id', 'first_name', 'last_name', 'birth_date', aquarium)        owner.add_fish('height', 'color', 'species', 'gender')        owner.add_fish('height', 'color', 'species', 'gender')        owner.add_fish('height', 'color', 'species', 'gender')        returned = owner.has_two_fish()        assert returned == Falseif __name__ == '__main__':    pytest.main(['-v', '-s', 'question00.py'])
